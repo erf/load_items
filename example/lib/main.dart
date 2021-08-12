5b0e89a612d35d8d09942ea0015b4c3ad54b3867
Erlend Lind Madsen <erlendf80@gmail.com>
2021-08-12 22:27:45 +0200

    add example

---
+++ example/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:load_more/load_more.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Item {
+  int index;
+  Item(this.index);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'load_more',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('load_more'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.list)),
+              Tab(icon: Icon(Icons.grid_3x3)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            LoadMore<Item>(
+              loadMoreType: LoadMoreType.list,
+              itemBuilder: itemBuilder,
+              itemsLoader: itemsLoader,
+            ),
+            LoadMore<Item>(
+              loadMoreType: LoadMoreType.grid,
+              itemBuilder: itemBuilder,
+              itemsLoader: itemsLoader,
+              gridCrossAxisCount: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget itemBuilder(BuildContext ctx, item, int index) {
+    return ListTile(
+      title: SizedBox(
+        width: 100,
+        height: 100,
+        child: ColoredBox(
+          color: Colors.redAccent,
+          child: Center(
+            child: Text('Item ${item.index}'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<List<Item>> itemsLoader(List currentItems) async {
+    await Future.delayed(const Duration(milliseconds: 750));
+    return List.generate(16, (i) => Item(currentItems.length + i));
+  }
+}
